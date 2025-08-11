041b608ae42e454d3515e4c75b0e5ca4c61e8aa2
ismoil77 <braboev@gmail.com>
2025-08-11 15:56:26 +0500

    dsdasdasd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   server: {
+    proxy: {
+      '/api': {
+        target: 'http://37.27.29.18:8001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
